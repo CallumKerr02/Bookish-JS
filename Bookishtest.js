1c85a37177a836651b91e77e1c2ce68ce277acf0
Slurp5 <60278250+Slurp5@users.noreply.github.com>
2021-10-14 14:50:51 +0100

    Login attempted implementation

--- Bookishtest.js
+++ Bookishtest.js
@@ -6,7 +6,7 @@ const app = express();
 const pgp = require('pg-promise')();
 const password = require('./config');
 
-const db = pgp(`postgres://postgres:${password}@localhost:5432/Bookish`);
+const db = pgp(`postgres://postgres:${password}@localhost:5432/postgres`);
 
 class Bookdata{
     constructor(id, name, isbn, barCode, authorId){
@@ -38,7 +38,33 @@ app.get("/books", (req, res)=>{
     });
 });
 
+app.get("/login", (req, res) => {
+    db.any("SELECT * FROM ACCOUNT")
+    .then((data) => {
+        const name = request.query.name;
+        const password = request.query.password;
+
+            for (let i = 0 ; i < data.length ; i++){
+                if (name === true && password === true){
+                    res.send({
+                        message: `Hello ${name}!`,
+                        token: createTokenForUser(user)
+                    });
+                }
+                else{
+                    res.status(400).send({
+                        errors: "Wrong username and password entered"
+                    });
+                }
+            }
+    })
+}
+)
+
 app.listen(3000, () => {
     console.log("Bookish is on port 3000 :)");
-
 });
+
+function createTokenForUser(name){
+    return jwt.sign({name: name}, secret);
+}
